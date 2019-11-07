ddaddd45c92719be48a76427e35dcd42e6f23899
Rezaem <rezanina.emil@gmail.com>
2019-11-07 07:41:48 +0100

    Mars Rover kata - solution

---
+++ session-20191105/session-20191105.tests/MarsRoverTestCases.fs
@@ -0,0 +1,53 @@
+﻿
+module MarsRoverTestCases
+
+open NUnit.Framework
+open session_20191105.mars_rover
+
+type TestSourceRecord = { start: State; commands : string Option; expected : State}
+
+[<TestFixture>]
+type ``Move Tesst Cases``() =
+    static member ForwardMoveTestCaseSources = 
+        [
+        { start = ((0, 0), North); commands = (Option.Some "f"); expected = ((1, 0), North) }
+        { start = ((1, 0), South); commands = (Option.Some "f"); expected = ((0, 0), South) }
+        { start = ((0, 0), East); commands = (Option.Some "f"); expected = ((0, 1), East) }
+        { start = ((0, 1), West); commands = (Option.Some "f"); expected = ((0, 0), West) }
+        ]
+
+    static member BackwardMoveTestCaseSources = 
+        [
+        { start = ((1, 0), North); commands = (Option.Some "b"); expected = ((0, 0), North) }
+        { start = ((0, 0), South); commands = (Option.Some "b"); expected = ((1, 0), South) }
+        { start = ((0, 1), East); commands = (Option.Some "b"); expected = ((0, 0), East) }
+        { start = ((0, 0), West); commands = (Option.Some "b"); expected = ((0, 1), West) }
+        ]
+
+    static member MultipleMoveTestCaseSources = 
+        [
+        { start = ((1, 0), North); commands = (Option.Some "bf"); expected = ((1, 0), North) }
+        { start = ((0, 0), South); commands = (Option.Some "bf"); expected = ((0, 0), South) }
+        { start = ((0, 1), East); commands = (Option.Some "bf"); expected = ((0, 1), East) }
+        { start = ((0, 0), West); commands = (Option.Some "bf"); expected = ((0, 0), West) }
+        { start = ((1, 0), North); commands = (Option.Some "bfl"); expected = ((1, 0), West) }
+        { start = ((1, 0), North); commands = (Option.Some "bfrf"); expected = ((1, 1), East) }
+        { start = ((1, 0), North); commands = (Option.Some "bfrfr"); expected = ((1, 1), South) }
+        { start = ((1, 0), North); commands = (Option.Some "bfrfrb"); expected = ((2, 1), South) }
+        { start = ((1, 0), North); commands = (Option.Some "bfrfrbl"); expected = ((2, 1), East) }
+        ]
+
+    [<TestCaseSource("ForwardMoveTestCaseSources")>]
+    member x.``ForwardMoveTest`` (testData: TestSourceRecord) =
+        let actual = moveRover testData.start testData.commands
+        Assert.AreEqual(testData.expected, actual)
+
+    [<TestCaseSource("BackwardMoveTestCaseSources")>]
+    member x.``BackwardMoveTest`` (testData: TestSourceRecord) =
+        let actual = moveRover testData.start testData.commands
+        Assert.AreEqual(testData.expected, actual)
+
+    [<TestCaseSource("MultipleMoveTestCaseSources")>]
+    member x.``MultipleMoveTest`` (testData: TestSourceRecord) =
+        let actual = moveRover testData.start testData.commands
+        Assert.AreEqual(testData.expected, actual)
---
+++ session-20191105/session-20191105/Library.fs
@@ -0,0 +1,76 @@
+﻿namespace session_20191105
+
+
+
+module mars_rover =
+    type Position = int * int
+    type Direction =
+        | North
+        | South
+        | East
+        | West
+    type State = Position * Direction
+    type CommandType = | LeftRotation | RightRotation | MoveForward | MoveBackward
+
+    let leftRotation (direction : Direction) =
+        match direction with
+        | North -> West
+        | South -> East
+        | East -> North
+        | West -> South
+
+    let rightRotation (direction : Direction) =
+        match direction with
+        | North -> East
+        | South -> West
+        | East -> South
+        | West -> North
+    
+    let moveVertical (position : Position) step =
+        fst position + step, snd position
+    
+    let moveHorizontal (position : Position) step =
+        fst position, snd position + step
+
+
+    let move (state : State) step =
+        match snd state with
+        | North -> moveVertical (fst state) step, snd state
+        | South -> moveVertical (fst state) -step, snd state
+        | East -> moveHorizontal (fst state) step, snd state
+        | West -> moveHorizontal (fst state) -step, snd state
+            
+    let moveForward (state : State) = move state 1
+
+    let moveBackward (state : State) = move state -1
+
+    let commandMap = fun character ->
+        match character with
+        | 'l' -> LeftRotation
+        | 'r' -> RightRotation
+        | 'f' -> MoveForward
+        | 'b' -> MoveBackward
+        | _ -> failwith "Invalid command"
+
+    let transformInput (input : string) =
+        List.map commandMap (Seq.toList input)
+
+    let execute (state: State) command =
+        match command with
+        | LeftRotation -> fst state, leftRotation (snd state)
+        | RightRotation -> fst state, rightRotation (snd state)
+        | MoveForward -> moveForward state
+        | MoveBackward -> moveBackward state
+
+    let rec run state commands =
+        match commands with
+        | [] -> state
+        | head::tail ->
+            let newState = execute state head
+            run newState tail
+
+    let moveRover (start : State) (path : string Option) : State = 
+        match path with
+        | Some path -> run start (transformInput  path)
+        | None -> start    
+    
